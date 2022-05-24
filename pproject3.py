13ff1de7a02880eb9402caaf169b1e3fac91a949
Mouad Moussabbih <41213132+MoMoussabbih@users.noreply.github.com>
2022-05-24 21:38:18 +0200

    Update pproject3.py

--- pproject3.py
+++ pproject3.py
@@ -89,7 +89,7 @@ if uploaded_file:
         st.metric("Mean squared error", value=f"{mean_squared_error(y_test, reg.predict(X_test)):.7g}")
     
     st.write("##### Model intercepts and coefficients") 
-    coefficients = list(reg.intercept_).extend(list(reg.coef_))
+    coefficients = [list(reg.intercept_).extend(list(reg.coef_))] 
     coef_names = ["b0"].extend([f"b{i}" for i in range(1,len(coefficients)+1)]) 
     df2 = pd.Dataframe(coefficients, columns = coef_names) 
-    st.dataframe(df2)
+    st.dataframe(df2) 
